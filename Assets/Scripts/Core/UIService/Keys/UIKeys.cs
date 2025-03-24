ef91cd3fc8dd710152e0929baddb733eb449df35
cnkrtnmai <79909883+cnkrtn@users.noreply.github.com>
2025-03-24 16:38:14 +0300

    .
    .

--- Assets/Scripts/Core/UIService/Keys/UIKeys.cs
+++ Assets/Scripts/Core/UIService/Keys/UIKeys.cs
@@ -2,50 +2,17 @@ namespace Core.UIService.Keys
 {
     public abstract class UIKeys
     {
-        public const string KEY_MERCHANT_UI = "UIMerchant";
-        public const string KEY_GAME_RESULT_UI = "UIGameResult";
-        public const string KEY_GUILD_UI = "UIGuild";
-        public const string KEY_ARENA_UI = "UIArena";
-        public const string KEY_ARENA_OPPONENT_UI = "UIArenaOpponent";
-        public const string KEY_EVENTS_UI = "UIEvents";
-        public const string KEY_TOWN_UI = "UITown";
-        public const string KEY_SHOP_UI = "UIShop";
-        public const string KEY_TAVERN_UI = "UITavern";
-        public const string KEY_RAIDS_UI = "UIRaids";
-        public const string KEY_WORLD_UI = "UIWorld";
-        public const string KEY_EVENT_STAGES_UI = "UIEventStages";
-        public const string KEY_AUTH_UI = "UIAuth";
-        public const string KEY_ARENA_BLOCK_UI = "UIArenaBlock";
-        public const string KEY_EDIT_DEFENSE_TEAM_UI = "UIEditDefenseTeam";
-        public const string KEY_GUILD_INVITE_UI = "UIGuildInvite";
+       
         public const string KEY_POPUP_UI = "UIPopUp";
-        public const string KEY_RAID_BASE_UI = "UIRaidBase";
+     
         public const string KEY_RETURN_POPUP_UI = "UIReturnPopUp";
-        public const string KEY_HERO_SUMMON_SCREEN_UI = "UIHeroSummonScreen";
+       
         public const string KEY_LOADING_SCREEN_UI = "UILoadingScreen";
         public const string KEY_LOADING_SCREEN_SMALL_UI = "UILoadingScreenSmall";
         public const string KEY_FADE_UI = "UIFade";
-        public const string KEY_BOTTOM_NAVIGATION_BAR = "UIBottomNavigationBar";
-        public const string KEY_TOP_BAR_UI = "UITopBar";
-        public const string KEY_HERO_INVENTORY_UI = "UIHeroInventory";
-        public const string KEY_HERO_CARD_UI = "UIHeroCard";
-        public const string KEY_ENEMY_TOWN_DETAILS_PANEL_UI = "UIEnemyTownDetailsPanel";
-        public const string KEY_BATTLE_FORMATION_SELECT_PANEL_UI = "UIBattleFormationSelectPanel";
+       
         public const string KEY_SLIDER_UI = "UISlider";
-        public const string KEY_INVENTORY_UI = "UIInventory";
-        public const string KEY_BATTLE_UI = "UIBattle";
-        public const string KEY_GENERAL_UI = "UIGeneral";
-        public const string KEY_GUILD_REQUEST_LIST = "UIGuildRequestList";
-        public const string KEY_QUEST_UI = "UIQuest";
-        public const string KEY_STATUE_DESCRIPTION_UI = "UIStatueDescription";
-        public const string KEY_LEVEL_UP_UI = "UILevelUp";
-        public const string KEY_INFO_BOX_UI = "UIInfoBox";
-        public const string KEY_SKILL_INFO_BOX_UI = "UISkillInfoBox";
-        public const string KEY_ARE_YOU_SURE_UI = "UIAreYouSure";
-        public const string KEY_SUMMON_UI = "UISummon";
-        public const string KEY_PLAYER_PROFILE_UI = "UIPlayerProfile";
-        public const string KEY_BATTLE_PASS_UI = "UIBattlePass";
-        public const string KEY_AD_REWARDS_UI = "UIAdRewards";
+     
 
 
     }
